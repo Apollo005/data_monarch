a159d3a37b0c7f3b3ecbe8a97cbd54c90e1a33aa
Aditya <adityamtambe@gmail.com>
2025-03-08 10:50:35 -0600

    checking commits

--- main.py
+++ main.py
@@ -21,19 +21,25 @@ def read_root():
 @app.post("/upload/")
 async def upload_file(file: UploadFile = File(...)):
     
-    if file.filename.endswith('.csv', '.xlsx') :
+    if file.filename.endswith(('.csv', '.xlsx')) :
         try:
             content = await file.read()
             #check if the file is a csv data file
+            #try to work with weirdly formatted data: "skip headers"...
             if file.filename.endswith('.csv') :
                 df = pd.read_csv(io.StringIO(content.decode("utf-8")))
+                print('csv')
             #check if the file is a excel data file
             elif file.filename.endswith('.xlsx') :
-                df = pd.read_excel(io.StringIO(content.decode("utf-8")))
+                df = pd.read_excel(io.BytesIO(content))
+                print('xlsx')
             #add json data upload functionality soon...
+                
+            df = df.replace({np.nan: None, np.inf: None, -np.inf: None})
 
         except Exception as e:
             raise HTTPException(status_code=500, detail=f"Error processing {file.filename}: {str(e)}")
-    
+    else :
+        raise HTTPException(status_code=415, detail=f"Unsupported file type for {file.filename}")
     # return {"message": "File received"}
     return JSONResponse({"message": "File processed", "data": df.to_dict()})
