ad82cc8ef18f15c218dcf00312c0be15194ba8ff
Aditya <adityamtambe@gmail.com>
2025-03-08 15:28:09 -0600

    checking commits

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ def read_root():
 # def read_item(item_id: int, q: str):
 #     return {"item_id": item_id, "q": q}
 
+#drawback of cleaning functions is it only checks for the first row that has the same # of cols as the max number of cols in the dataset
+
 def clean_header_csv(content):
     lines = content.split("\n")
     for i, line in enumerate(lines):
@@ -25,6 +27,20 @@ def clean_header_csv(content):
         if len(cols) > 1 and all(len(col.strip()) > 0 for col in cols):
             return i
     return 0
+
+def clean_header_xlsx(content):
+    #assuming no headers and every row is captured
+    df = pd.read_excel(io.BytesIO(content), header=None)
+    max_cols = max(df.apply(lambda row: row.dropna().shape[0], axis=1))
+
+    for i in range(len(df)):
+        row = df.iloc[i].dropna().astype(str).str.strip()
+
+        if len(row) == max_cols:
+            return i
+        
+    return 0
+
 
 # example of the skip header functionality:
 # file_path = "/Users/adity1/Desktop/Climate 323/Lab01/CSRB_20040716.csv"
@@ -39,17 +55,19 @@ async def upload_file(file: UploadFile = File(...)):
     if file.filename.endswith(('.csv', '.xlsx')) :
         try:
             content_bytes = await file.read()
-            content_str = content_bytes.decode("utf-8")
+
             #check if the file is a csv data file
-            #try to work with weirdly formatted data: "skip headers"...
             if file.filename.endswith('.csv') :
+                content_str = content_bytes.decode("utf-8")
                 #if there are extra rows that need to be skipped this will do that
                 skip_rows = clean_header_csv(content_str)
                 df = pd.read_csv(io.StringIO(content_str), skiprows=skip_rows)
                 print('csv')
+
             #check if the file is a excel data file
             elif file.filename.endswith('.xlsx') :
-                df = pd.read_excel(io.BytesIO(content_bytes))
+                skip_rows = clean_header_xlsx(content_bytes)
+                df = pd.read_excel(io.BytesIO(content_bytes), skiprows=skip_rows)
                 print('xlsx')
             #add json data upload functionality soon...
                 
