78752387a7c5735e67c31d7a2f7487b7ab4cd76d
Aditya <adityamtambe@gmail.com>
2025-03-08 12:29:57 -0600

    checking commits

--- main.py
+++ main.py
@@ -18,20 +18,38 @@ def read_root():
 # def read_item(item_id: int, q: str):
 #     return {"item_id": item_id, "q": q}
 
+def clean_header_csv(content):
+    lines = content.split("\n")
+    for i, line in enumerate(lines):
+        cols = line.split(",")
+        if len(cols) > 1 and all(len(col.strip()) > 0 for col in cols):
+            return i
+    return 0
+
+# example of the skip header functionality:
+# file_path = "/Users/adity1/Desktop/Climate 323/Lab01/CSRB_20040716.csv"
+# with open(file_path, "r") as f:
+#     content = f.read()
+# skip_rows = clean_header_csv(content)
+# print(skip_rows)
+
 @app.post("/upload/")
 async def upload_file(file: UploadFile = File(...)):
     
     if file.filename.endswith(('.csv', '.xlsx')) :
         try:
-            content = await file.read()
+            content_bytes = await file.read()
+            content_str = content_bytes.decode("utf-8")
             #check if the file is a csv data file
             #try to work with weirdly formatted data: "skip headers"...
             if file.filename.endswith('.csv') :
-                df = pd.read_csv(io.StringIO(content.decode("utf-8")))
+                #if there are extra rows that need to be skipped this will do that
+                skip_rows = clean_header_csv(content_str)
+                df = pd.read_csv(io.StringIO(content_str), skiprows=skip_rows)
                 print('csv')
             #check if the file is a excel data file
             elif file.filename.endswith('.xlsx') :
-                df = pd.read_excel(io.BytesIO(content))
+                df = pd.read_excel(io.BytesIO(content_bytes))
                 print('xlsx')
             #add json data upload functionality soon...
                 
