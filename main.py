081f08e9a63891a57b4e33dedf0773b70b87bf7e
Aditya <adityamtambe@gmail.com>
2025-03-08 09:54:41 -0600

    updated main.py with fastapi upload functionality

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from fastapi import FastAPI, UploadFile, File, Form
+from fastapi import FastAPI, UploadFile, File, Form, HTTPException
 from typing import Union
 import io
 from fastapi.responses import JSONResponse, FileResponse
@@ -20,8 +20,20 @@ def read_root():
 
 @app.post("/upload/")
 async def upload_file(file: UploadFile = File(...)):
-    content = await file.read()
-    df = pd.read_csv(io.StringIO(content.decode("utf-8")))
+    
+    if file.filename.endswith('.csv', '.xlsx') :
+        try:
+            content = await file.read()
+            #check if the file is a csv data file
+            if file.filename.endswith('.csv') :
+                df = pd.read_csv(io.StringIO(content.decode("utf-8")))
+            #check if the file is a excel data file
+            elif file.filename.endswith('.xlsx') :
+                df = pd.read_excel(io.StringIO(content.decode("utf-8")))
+            #add json data upload functionality soon...
+
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Error processing {file.filename}: {str(e)}")
     
     # return {"message": "File received"}
     return JSONResponse({"message": "File processed", "data": df.to_dict()})
