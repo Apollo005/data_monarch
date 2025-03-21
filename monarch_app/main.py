29a9f69f4be1086d38cf871c81cf620d5fb27840
Aditya <adityamtambe@gmail.com>
2025-03-21 19:02:49 -0400

    git testing

--- monarch_app/main.py
+++ monarch_app/main.py
@@ -15,7 +15,6 @@ from sqlalchemy.ext.declarative import declarative_base
 
 app = FastAPI()
 
-print("hi")
     
 def create_table_from_df(df, table_name, cursor):
     # Optionally reset index and add a primary key column if desired:
