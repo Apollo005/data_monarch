48dcd4661020e3470c4a64232af453aa89a4cb88
Aditya <adityamtambe@gmail.com>
2025-03-30 02:35:19 -0400

    updated import for create db

--- monarch_app/backend/database/create_db.py
+++ monarch_app/backend/database/create_db.py
@@ -1,6 +1,6 @@
 from utils.imports import *
 from database.tables import Base
-from database.user_database import DATABASE_URL
+from database.users import DATABASE_URL
 
 engine = create_engine(DATABASE_URL)
 Base.metadata.create_all(bind=engine)
